76176bb20d3f33e98cc14ff57143fb33b5f2a787
Guilherme Candida <guilcodez@gmail.com>
2026-06-02 19:19:26 -0300

    feat: criando as classes Jogador e Tecnico herdando de Pessoa

--- src/model/Jogador.java
+++ src/model/Jogador.java
@@ -1,4 +1,34 @@
 package model;
 
-public class Jogador {
+public class Jogador extends Pessoa {
+    private static final long serialVersionUID = 1L;
+
+    private String posicao;
+    private int numeroCamisa;
+    private int golsMarcados;
+
+    // O construtor usa o super() para passar o nome e idade para a classe mãe (Pessoa)
+    public Jogador(String nome, int idade, String posicao, int numeroCamisa) {
+        super(nome, idade);
+        this.posicao = posicao;
+        this.numeroCamisa = numeroCamisa;
+        this.golsMarcados = 0; // To do jogador começa o campeonato com 0 gols
+    }
+
+    // Getters e Setters
+    public String getPosicao() { return posicao; }
+    public void setPosicao(String posicao) { this.posicao = posicao; }
+
+    public int getNumeroCamisa() { return numeroCamisa; }
+    public void setNumeroCamisa(int numeroCamisa) { this.numeroCamisa = numeroCamisa; }
+
+    public int getGolsMarcados() { return golsMarcados; }
+    public void setGolsMarcados(int golsMarcados) { this.golsMarcados = golsMarcados; }
+
+    // O método toString ajuda a exibir o jogador de forma elegante no console depois
+    @Override
+    public String toString() {
+        return String.format("Jogador: %s (#%d) | Posição: %s | Gols: %d",
+                getNome(), numeroCamisa, posicao, golsMarcados);
+    }
 }
--- src/model/Tecnico.java
+++ src/model/Tecnico.java
@@ -1,4 +1,23 @@
 package model;
 
-public class Tecnico {
+public class Tecnico extends Pessoa {
+    private static final long serialVersionUID = 1L;
+
+    private String esquemaTaticoFavorito;
+
+    public Tecnico(String nome, int idade, String esquemaTaticoFavorito) {
+        super(nome, idade);
+        this.esquemaTaticoFavorito = esquemaTaticoFavorito;
+    }
+
+    // Getters e Setters
+    public String getEsquemaTaticoFavorito() { return esquemaTaticoFavorito; }
+    public void setEsquemaTaticoFavorito(String esquemaTaticoFavorito) {
+        this.esquemaTaticoFavorito = esquemaTaticoFavorito;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Técnico: %s | Esquema Favorito: %s", getNome(), esquemaTaticoFavorito);
+    }
 }
